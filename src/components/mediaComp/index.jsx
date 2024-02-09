ee3cd8d11c51dfa46e1e6f20512b65a075676281
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-02-09 09:07:04 +0500

    connected apis to media page

--- src/components/mediaComp/index.jsx
+++ src/components/mediaComp/index.jsx
@@ -1,49 +1,47 @@
-import "./media.css"
-import MediaImg from "../../assets/img/property-03.jpg"
+import "./media.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const MediaComp = () => {
+  const [photo, setPhoto] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(9);
+
+  useEffect(() => {
+    fetch(
+      import.meta.env.VITE_APP_BASE_URL + `/img?page=${page}&limit=${limit}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => setPhoto(data.results));
+  }, [page, limit]);
+
+  const increment = () => {
+    setLimit(limit + 9)
+  }
+
   return (
     <section className="media">
       <div className="container">
         <div className="media-inner">
-          <h2 className="media-title">
-            Rasmlar
-          </h2>
+          <h2 className="media-title">Rasmlar</h2>
 
           <ul className="media-list">
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
-            <li className="media-item">
-              <img src={MediaImg} alt="img" className="media-img" />
-            </li>
+            {photo.length &&
+              photo.map((item, idx) => (
+                <li className="media-item" key={idx}>
+                  <img src={item.img} alt="img" className="media-img" />
+                </li>
+              ))}
           </ul>
-          <button className="media-btn">
-            Yana ko'rish
-          </button>
+          <button className="media-btn" onClick={increment}>Yana ko'rish</button>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
